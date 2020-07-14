11c36ac604d058f022b46ea35ca8ce6644a28ff6
Matt Martindale <martindalematt18@yahoo.com>
2020-07-13 21:33:48 -0600

    Added project files

---
+++ VideoTimeline/VideoTimeline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoTimeline
+//
+//  Created by Matthew Martindale on 7/13/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
